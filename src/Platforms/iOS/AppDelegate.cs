8a10e7dd732e927a1431aaced49ecf6bc3183073
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2023-05-16 20:34:00 +0100

    Removed unsupported platform files

--- src/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Glyphy;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
