3621e704cb1c909bc6ee1615cb7f6d566044475a
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2023-06-25 21:45:10 +0100

    Added process limit hints to the settings page

--- src/Platforms/Android/Views/SettingsPage.cs
+++ src/Platforms/Android/Views/SettingsPage.cs
@@ -1,4 +1,6 @@
-﻿using Glyphy.Platforms.Android;
+﻿using Android.Content;
+using Glyphy.Platforms.Android;
+using Microsoft.Maui.ApplicationModel;
 using System;
 
 namespace Glyphy.Views
@@ -9,6 +11,20 @@ namespace Glyphy.Views
         {
             Header.Padding = new(Header.Padding.Left, /*Header.Padding.Top + */Helpers.StatusBarHeight, Header.Padding.Right, Header.Padding.Bottom);
             Padding = new(Padding.Left, Padding.Top, Padding.Right, /*Padding.Bottom + */Helpers.NavigationBarHeight);
+        }
+
+        private void AboutDeviceButton_Clicked(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(Android.Provider.Settings.ActionDeviceInfoSettings);
+            //TODO: See if I can navigate directly to the "Software information" page.
+            Platform.CurrentActivity?.StartActivity(intent);
+        }
+
+        private void DeveloperProcessOptionsButton_Clicked(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(Android.Provider.Settings.ActionApplicationDevelopmentSettings);
+            //TODO: See if I can have this scroll down to the "Background process limit" option.
+            Platform.CurrentActivity?.StartActivity(intent);
         }
     }
 }
