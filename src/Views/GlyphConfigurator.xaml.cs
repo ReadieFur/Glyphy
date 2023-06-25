7dd95b939ecff6c2edcc9ea53484cbf6751617eb
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2023-06-26 00:10:09 +0100

    Added multi-frame support to the UI

--- src/Views/GlyphConfigurator.xaml.cs
+++ src/Views/GlyphConfigurator.xaml.cs
@@ -5,11 +5,13 @@ using Glyphy.Configuration;
 using Glyphy.LED;
 using Glyphy.Misc;
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Glyphy.Views;
 
@@ -59,12 +61,14 @@ public partial class GlyphConfigurator : ContentPage, IDisposable
 #endif
     }
 
-    //TODO: Speed up this constructor as it takes too long to load.
     private void SharedConstructor()
     {
         InitializeComponent();
 
         ToggleControls(false);
+
+        for (uint i = 0; i < _animation.Frames.Count; i++)
+            FrameList.Add(CreateFrameButton(i));
 
         //These get added in order of the enum so we can use the index to get the enum value (though to be safe I will re-format them).
         foreach (string key in Enum.GetNames<EAddressable>())
@@ -199,14 +203,34 @@ public partial class GlyphConfigurator : ContentPage, IDisposable
             }
         });
 
-        //TODO: Update the UI to reflect the new frame.
-        if (_animation.Frames[currentFrameIndex].Values.Count == 0)
-            return;
+        foreach (Button frameButton in FrameList)
+            frameButton.SetAppTheme(Button.BackgroundColorProperty, Color.FromArgb("#F2F2F2"), Color.FromArgb("#0D0D0D"));
+        ((Button)FrameList[(int)frameIndex]).BackgroundColor = Colors.Transparent;
 
-        KeyValuePair<EAddressable, SLEDValue> firstLED = _animation.Frames[currentFrameIndex].Values.First();
-        LEDPicker.SelectedIndex = (int)firstLED.Key;
-        brightnessInputControlSyncer.SetValue(Math.Round(firstLED.Value.Brightness * 100, 1));
-        InterpolationPicker.SelectedIndex = (int)firstLED.Value.InterpolationType;
+        SLEDValue ledValue;
+        if (_animation.Frames[currentFrameIndex].Values.Count > 0)
+        {
+            KeyValuePair<EAddressable, SLEDValue> firstLED = _animation.Frames[currentFrameIndex].Values.First();
+            ledValue = firstLED.Value;
+            LEDPicker.SelectedIndex = (int)firstLED.Key;
+        }
+        else
+        {
+            ledValue = new();
+            LEDPicker.SelectedIndex = 0;
+        }
+        brightnessInputControlSyncer.SetValue(Math.Round(ledValue.Brightness * 100, 1));
+        InterpolationPicker.SelectedIndex = (int)ledValue.InterpolationType;
+    }
+
+    private Button CreateFrameButton(uint index)
+    {
+        Button frameButton = new();
+        frameButton.Text = $"{index + 1}";
+        frameButton.Clicked += (_, _) => LoadFrame(uint.Parse(frameButton.Text) - 1);
+        frameButton.SetAppTheme(Button.TextColorProperty, Colors.Black, Colors.White);
+        frameButton.SetAppTheme(Button.BackgroundColorProperty, Color.FromArgb("#F2F2F2"), Color.FromArgb("#0D0D0D"));
+        return frameButton;
     }
     #endregion
 
@@ -310,6 +334,52 @@ public partial class GlyphConfigurator : ContentPage, IDisposable
         return roundedValue;
     }
 
+    /// <summary>
+    /// Deletes the currently selected frame.
+    /// </summary>
+    private void DeleteFrameButton_Clicked(object sender, EventArgs e)
+    {
+        _animation.Frames.RemoveAt(currentFrameIndex);
+        FrameList.RemoveAt(currentFrameIndex);
+
+        //Shift all of the UI button values on the right of the deleted frame down by one.
+        for (int i = currentFrameIndex; i < FrameList.Count; i++)
+            ((Button)FrameList[i]).Text = $"{i + 1}";
+
+        //Modulo cannot be used here in-case we are deleting the last frame (division by zero).
+        int newActiveFrameIndex = currentFrameIndex - 1;
+
+        //If the new active frame index is less than zero, and there are no more frames, create a new empty frame.
+        if (newActiveFrameIndex < 0 && _animation.Frames.Count == 0)
+        {
+            _animation.Frames.Add(new());
+            FrameList.Add(CreateFrameButton(0));
+            newActiveFrameIndex = 0;
+        }
+        //Otherwise, if the new active frame index is less than zero, set it to zero.
+        else if (newActiveFrameIndex < 0)
+        {
+            newActiveFrameIndex = 0;
+        }
+
+        LoadFrame((uint)newActiveFrameIndex);
+    }
+
+    /// <summary>
+    /// Adds a new frame to the right of the currently selected frame.
+    /// </summary>
+    private void AddFrameButton_Clicked(object sender, EventArgs e)
+    {
+        _animation.Frames.Insert(currentFrameIndex + 1, new());
+        FrameList.Insert(currentFrameIndex + 1, CreateFrameButton((uint)currentFrameIndex + 1));
+
+        //Shift all of the UI button values on the right of the new frame up by one.
+        for (int i = currentFrameIndex + 1; i < FrameList.Count; i++)
+            ((Button)FrameList[i]).Text = $"{i + 1}";
+
+        LoadFrame((uint)currentFrameIndex + 1);
+    }
+
     private async void SaveButton_Clicked(object sender, EventArgs e)
     {
         try
@@ -333,7 +403,7 @@ public partial class GlyphConfigurator : ContentPage, IDisposable
         if (!OnBackButtonPressed())
             Navigation.PopAsync();
     }
-#endregion
+    #endregion
 
     #region Overrides
     protected override bool OnBackButtonPressed()
