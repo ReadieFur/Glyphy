843c643cfed96c7784f137c745f388913d35fdc4
Chloe (Tristan) Read <60425965+ReadieFur@users.noreply.github.com>
2025-05-13 13:40:40 +0100

    Misc classes

---
+++ src/Misc/AutoDictionaryInitializer.cs
@@ -0,0 +1,19 @@
+﻿namespace Glyphy.Misc
+{
+    public class AutoDictionaryInitializer<TKey, TValue> : Dictionary<TKey, TValue> where TKey : notnull where TValue : new()
+    {
+        public new TValue this[TKey key]
+        {
+            get
+            {
+                if (!TryGetValue(key, out TValue? value))
+                {
+                    value = new TValue();
+                    Add(key, value);
+                }
+                return value;
+            }
+            set => base[key] = value;
+        }
+    }
+}
